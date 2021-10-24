6566c0eebf4f5d778e866846de08e5df660de068
ATINDRA PAL <atindra8016@gmail.com>
2021-10-24 23:04:55 +0530

    Add Word Count feature

--- viceversa/views.py
+++ viceversa/views.py
@@ -1,3 +1,4 @@
+from django.http.request import split_domain_port
 from django.shortcuts import render
 
 def home(requests):
@@ -6,4 +7,6 @@ def home(requests):
 def reverse(requests):
     get_text = requests.GET['usertext']
     reverse_text = get_text[: : -1]
-    return render(requests, 'reverse.html', {'usertext': get_text, 'reversed_text': reverse_text})
+    splitted_word = get_text.split()
+    word_len = len(splitted_word)
+    return render(requests, 'reverse.html', {'usertext': get_text, 'reversed_text': reverse_text, 'wordcount': word_len})
